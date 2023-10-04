e1a68040785109e308a1b311c9ec6497fa928e98
htxs <gzucm_tianjie@foxmail.com>
2023-10-04 22:23:38 +0800

    08-queues: QueueRingBuffer

--- 08-queues/Queue.playground/Contents.swift
+++ 08-queues/Queue.playground/Contents.swift
@@ -31,3 +31,20 @@ example(of: "QueueLinkedList") {
     queue.dequeue()
     print("After dequeue elements: \(queue)")
 }
+
+example(of: "QueueRingBuffer") {
+    var queue = QueueRingBuffer<String>(count: 10)
+    queue.enqueue("Ray")
+    queue.enqueue("Brian")
+    queue.enqueue("Eric")
+    
+    print("Queue elements: \(queue)")
+    
+    if let peekedValue = queue.peek {
+        assert("Ray" == peekedValue)
+        print("Peeked: \(peekedValue)")
+    }
+    
+    queue.dequeue()
+    print("After dequeue elements: \(queue)")
+}
