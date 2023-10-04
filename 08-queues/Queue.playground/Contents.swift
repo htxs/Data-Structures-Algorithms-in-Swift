8bc70cb6774c239d9249f461bc9ba137efbd92d0
htxs <gzucm_tianjie@foxmail.com>
2023-10-04 23:09:36 +0800

    08-queues: QueueStack

--- 08-queues/Queue.playground/Contents.swift
+++ 08-queues/Queue.playground/Contents.swift
@@ -48,3 +48,20 @@ example(of: "QueueRingBuffer") {
     queue.dequeue()
     print("After dequeue elements: \(queue)")
 }
+
+example(of: "QueueStack") {
+    var queue = QueueStack<String>()
+    queue.enqueue("Ray")
+    queue.enqueue("Brian")
+    queue.enqueue("Eric")
+    
+    print("Queue elements: \(queue)")
+    
+    if let peekedValue = queue.peek {
+        assert("Ray" == peekedValue)
+        print("Peeked: \(peekedValue)")
+    }
+    
+    queue.dequeue()
+    print("After dequeue elements: \(queue)")
+}
